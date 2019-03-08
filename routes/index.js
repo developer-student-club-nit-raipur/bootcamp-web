d4470b6b142bc772eff6974041ab1253533f2f1c
shreyanshs7 <shreyanshss7@gmail.com>
2019-03-08 14:55:29 +0530

    Minor change to template

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var helpers = require('../helpers/jwt');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var token = helpers.createToken({ some : "some"});
-  console.log(token);
-  console.log(helpers.verifyToken(token));
   res.render('index', { title: 'Express' });
 });
 
