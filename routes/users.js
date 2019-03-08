ab59430488faf65290b5a11f9a694b34fbd1b083
shreyanshs7 <shreyanshss7@gmail.com>
2019-03-09 00:19:47 +0530

    Added basic features

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/form', function(req, res, next) {
+  res.render('form');
+});
+
+//POST Methods for getting form data
+router.post('/formpost', function(req, res){
+  var name = req.body.name;
+  var age = req.body.age;
+  var contact = req.body.contact;
+  
+  var userData = {
+    name : name,
+    age : age,
+    contact : contact
+  };
+  console.log(userData);
+  res.render('index');
+});
+
+router.get('/:name', (req, res) => {
+  var name = req.params.name;
+  console.log(name);
+  res.render('index');
 });
 
 module.exports = router;
