487e3d1934b990b4881ffe81776fc4cc47d51ca2
shreyanshs7 <shreyanshss7@gmail.com>
2019-03-09 00:47:42 +0530

    Added loops in ejs

--- routes/users.js
+++ routes/users.js
@@ -7,19 +7,19 @@ router.get('/form', function(req, res, next) {
 });
 
 //POST Methods for getting form data
-router.post('/formpost', function(req, res){
-  var name = req.body.name;
-  var age = req.body.age;
-  var contact = req.body.contact;
+// router.post('/formpost', function(req, res){
+//   var name = req.body.name;
+//   var age = req.body.age;
+//   var contact = req.body.contact;
   
-  var userData = {
-    name : name,
-    age : age,
-    contact : contact
-  };
-  console.log(userData);
-  res.render('data', { user : userData });
-});
+//   var userData = {
+//     name : name,
+//     age : age,
+//     contact : contact
+//   };
+//   console.log(userData);
+//   res.render('data', { user : userData });
+// });
 
 router.get('/:name', (req, res) => {
   var name = req.params.name;
@@ -27,5 +27,22 @@ router.get('/:name', (req, res) => {
   res.render('name', { name : name });
 });
 
+router.get('/loop/data', (req, res) => {
+  var users = [
+    {
+      name : "test 1",
+      age : "18"
+    },
+    {
+      name : "test 2",
+      age : "20"
+    },
+    {
+      name : "test 3",
+      age : "18"
+    },
+  ];
+  res.render('data', { users : users });
+});
 
 module.exports = router;
